b1f68aaf6a965dc5efaa614e45ee3da1c58a072d
Kenneth Atria <kennethatria@gmail.com>
2019-01-22 13:55:29 +0300

    fix: coordinates were not displaying on the map after map activity launched

--- app/src/main/java/com/kennethatria/traveller/traveller/MapsActivity.java
+++ app/src/main/java/com/kennethatria/traveller/traveller/MapsActivity.java
@@ -22,19 +22,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     double origin_latitude,origin_longtitude,destination_latitude,destination_longtitude;
+    Intent intent;
+    Bundle extras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         /** retriving origin and destination coorginates **/
 
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
+        intent = getIntent();
+        extras = intent.getExtras();
+
         origin_latitude = extras.getDouble("origin_latitude");
         origin_longtitude = extras.getDouble("origin_longtitude");
         destination_latitude = extras.getDouble("destination_latitude");
@@ -42,37 +50,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         /** end lat and long coordinates **/
 
-        /** enabling back button on map activity **/
-
-
-        getActionBar().setDisplayHomeAsUpEnabled(true); // In `OnCreate();`
-
-
-
-
-
-
-        /** end : back button **/
-    }
-    @Override
-    public boolean onNavigateUp(){
-        finish();
-        return true;
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        /**mMap = googleMap;
-
+        mMap = googleMap;
+/**
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -82,22 +65,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add polylines and polygons to the map. This section shows just
         // a single polyline. Read the rest of the tutorial to learn more.
-        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+        Polyline polyline = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .add(
-                        new LatLng( origin_latitude, origin_longtitude),
-                        //new LatLng(-34.747, 145.592),
-                        //new LatLng(-34.364, 147.891),
-                        //new LatLng(-33.501, 150.217),
+
+                        //Toast.makeText(MapsActivity.this, Double.valueOf(origin_latitude).toString()+' '+ Double.valueOf(origin_longtitude).toString()+' '+Double.valueOf(destination_latitude).toString()+' '+Double.valueOf(destination_longtitude).toString()+' ', Toast.LENGTH_LONG).show();
+                        new LatLng(origin_latitude, origin_longtitude),
                         new LatLng(destination_latitude, destination_longtitude)));
 
-
-        // Position the map's camera near Alice Springs in the center of Australia,
+        // Position the map's camera set to Africa,
         // and set the zoom factor so most of Australia shows on the screen.
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(8.7832, 34.50853), 4));
 
-        // Set listeners for click events.
-        //googleMap.setOnPolylineClickListener(this);
-        //googleMap.setOnPolygonClickListener(this);
     }
 }
